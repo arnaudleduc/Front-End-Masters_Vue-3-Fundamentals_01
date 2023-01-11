46efe15d1e0bf055115bf863aca2e44cfc72a0d9
Arnaud Leduc <arnaudleduc@mbp-darnaud.home>
2023-01-11 20:53:00 +0100

    commit before 4th exercise

---
+++ Front-End-Masters_Vue-3-Fundamentals_01/src/App.vue
@@ -0,0 +1,73 @@
+<script>
+export default {
+  data() {
+    return {
+      count: 10,
+      incrementAmount: 1,
+      message: "Hello it works!",
+      listOfNumbers: [1, 2, 3, 4, 5],
+    };
+  },
+  computed: {
+    displayTitle() {
+      if (this.count >= 20) {
+        return "Counter Standard - Very Long";
+      } else {
+        return "Counter Standard";
+      }
+    },
+  },
+  methods: {
+    incrementCount() {
+      this.count += this.incrementAmount;
+    },
+  },
+};
+</script>
+
+<template>
+  <h1>{{ displayTitle }}</h1>
+  <p>{{ count }}</p>
+  <button @click="incrementCount">Increment Count</button>
+  <div>
+    <label for="incrementAmount">Increment by:</label>
+    <input type="number" v-model="incrementAmount" />
+  </div>
+  <hr />
+  <p v-if="message.length % 2 === 0">Even: {{ message }}</p>
+  <p v-else>Odd: {{ message.toUpperCase() }}</p>
+  <ul>
+    <li v-for="(number, index) in listOfNumbers" :key="`number-${index}`">
+      {{ number }}
+    </li>
+  </ul>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
